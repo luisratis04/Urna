fde15d8c65d3c433b827ccdcbcea7f6943eb268e
Luis Antonio <luis.developer.ac@gmail.com>
2022-12-13 19:39:16 -0500

    envia o voto para o backend

--- main.js
+++ main.js
@@ -14,9 +14,25 @@ function addNumber(number) {
 }
 
 function clean() {
-  console.log("foo");
   currentNumber = "";
 
   document.querySelector("input#digito-1").removeAttribute("value");
   document.querySelector("input#digito-2").removeAttribute("value");
+}
+
+async function vote() {
+  if (currentNumber.length !== 2) return;
+
+  fetch("http://localhost:3333/votes", {
+    method: "POST",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      vote: currentNumber,
+    }),
+  });
+
+  clean();
 }
